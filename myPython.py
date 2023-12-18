52860b705c53f5b86dd8f4ed425724d6bc27558f
Ozodbek <devanabuildmuber@gmail.com>
2023-12-18 10:44:33 -0500

    My first Python lesson

---
+++ myPython.py
@@ -0,0 +1,77 @@
+#Printing ...
+#Comments ...
+#Console ...
+#Input ...
+
+
+"""
+Comment is written after #. 
+If you want to write a line of comments, 
+you can write your own line of comments after the #
+"""
+
+
+#If you want to write a multi-line comment """you can write your comment here"""!
+
+
+print("Hello World")
+print("Hello, my name is Devana.Developer")
+print("...you can enter as much text as you want here")
+
+
+print('Hello, I Love my Python')
+print("print('hi')")
+#I think you understood what I was trying to tell you above
+"""
+print(Hello)
+print("Hello)
+print(Hello")
+print("Hello"
+print"Hello")
+
+...these are wrong actions
+"""
+print("Hi!\nHi!")
+"""Here you can wrap text to the next line using \n!"""
+
+print("\n")
+
+print("Hello!       Hello!\n Hello!     Hello!\n   Hello! Hello!\n      Hello!")
+#Yes, you can draw a pattern too :)
+
+print("Hello" + "my beloved Python")
+#Let's throw some space in the text
+print("Hello " + "my beloved Python")
+print("Hello" + " my beloved Python")
+print("Hello" + " " + "my beloved Python")
+"""   :)   """
+
+input("What is your username? ")
+
+print("Hello " + input("What is your name?\n"))
+
+print("I will bring it now. Soon... :)" + input(" How many cups of coffee do you need? ") )
+
+print("\n")
+
+# ...len function!
+print(len("Devana.Developer"))
+print(len(input("What is your name? ")))
+
+"""
+As you understand, 
+here we have created a program 
+that will determine how many 
+characters your name consists of... :)
+"""
+
+
+
+
+
+
+
+
+
+
+
